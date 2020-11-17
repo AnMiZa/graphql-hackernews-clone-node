cb8c728569b03d0781d0324f68c4ed54d08cf0f0
Andrzej Zaborski <andrzejzab@gmail.com>
2020-11-17 14:59:52 +0100

    Added Prisma and SQLite Database created Link and User models

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { PrismaClient } = require('@prisma/client')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => context.prisma.link.findMany(),
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          description: args.description,
+          url: args.url,
+        }
+      })
+      return newLink
     }
   }
 }
 
+const prisma = new PrismaClient()
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma
+  },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
