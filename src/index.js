d335d2dbcac6d8f8537c9e0b4c7a7dc7fdc4c50e
Andrzej Zaborski <andrzejzab@gmail.com>
2020-11-23 16:57:52 +0100

    Added post and vote GraphQL subscriptions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer, PubSub } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 }
 
 const prisma = new PrismaClient()
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -20,7 +26,8 @@ const server = new GraphQLServer({
   context: request => {
     return {
       ...request,
-      prisma
+      prisma,
+      pubsub
     }
   },
 })
